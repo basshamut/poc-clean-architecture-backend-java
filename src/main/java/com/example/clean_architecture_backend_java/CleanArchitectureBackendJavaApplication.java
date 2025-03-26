f3978aa5e5f3e8c1eec1db50e9acc2d7f3da4acf
Jesus Fajardo <es.jfajardobass@gmail.com>
2025-03-26 15:00:02 +0100

    Initial commit

---
+++ src/main/java/com/example/clean_architecture_backend_java/CleanArchitectureBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.clean_architecture_backend_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchitectureBackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanArchitectureBackendJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/clean_architecture_backend_java/CleanArchitectureBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clean_architecture_backend_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchitectureBackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
